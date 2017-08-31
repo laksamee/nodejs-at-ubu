1c9ae10c9bd885973131497aa5af885ed66fac84
laksamee <laksamee.pr.57@ubu.ac.th>
2017-08-31 18:21:42 +0700

    3

--- nodejs-01/myfirst/index.js
+++ nodejs-01/myfirst/index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World! 5711403366');
+    res.end('Hello World! 5711403366 Laksamee Prathumwan');
 }).listen(8080); 
