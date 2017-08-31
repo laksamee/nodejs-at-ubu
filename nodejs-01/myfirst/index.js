a9f2533cffbe19da21b82c3c11fe2d23e314bcd0
laksamee <laksamee.pr.57@ubu.ac.th>
2017-08-31 18:18:10 +0700

    3

--- nodejs-01/myfirst/index.js
+++ nodejs-01/myfirst/index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end('Hello World! 5711403366');
 }).listen(8080); 
